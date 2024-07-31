5c12bc98b4c5fe946d5d15ccd9d7194cb0e89fea
SayaliNikumbh25 <sayalinikumbh7890@gmail.com>
2024-07-31 21:54:17 +0530

    initial commit

---
+++ server/routes/folder.js
@@ -0,0 +1,12 @@
+import express from "express"
+import folder from "../controllers/folder.js";
+import verifyToken from "../middlewares/verifyToken.js";
+const router = express.Router();
+
+router.get('/:userID',  folder.getFolder)
+
+router.post('/create',folder.createFolder);
+
+router.delete('/delete/:folderId',folder.deleteFolder);
+
+export default router
